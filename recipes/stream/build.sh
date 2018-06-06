ad78948e90e1c65235c4c496268ec9682cbb0af4
huidongchen <gjlmile@gmail.com>
2018-06-06 14:55:46 -0400

    add stream

--- recipes/stream/build.sh
+++ recipes/stream/build.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
-Rscript -e ‘install.packages(“devtools”,repos=“https://cran.rstudio.com“)’
-Rscript -e ‘options(unzip=“internal”);devtools::install_github(“Albluca/distutils”)’
-Rscript -e ‘options(unzip=“internal”);devtools::install_github(“Albluca/ElPiGraph.R”, ref = “Development”)’
+# Rscript -e ‘install.packages(“devtools”,repos=“https://cran.rstudio.com“)’
+# Rscript -e ‘options(unzip=“internal”);devtools::install_github(“Albluca/distutils”)’
+# Rscript -e ‘options(unzip=“internal”);devtools::install_github(“Albluca/ElPiGraph.R”, ref = “Development”)’
 
 
-Rscript -e ‘install.packages(“igraph”,repos=“https://cran.rstudio.com”)’ #not needed
-Rscript -e ‘install.packages(“KernSmooth”,repos=“https://cran.rstudio.com”)’ #not needed
-
-
-
-
-Rscript -e ‘source(“https://bioconductor.org/biocLite.R”);biocLite(“BiocInstaller”);biocLite(“BSgenome.Hsapiens.UCSC.hg19");biocLite(“TFBSTools”);BiocInstaller::biocLite(“GreenleafLab/chromVAR”)’
-
-pip install networkx==1.10
-pip install shapely
+# Rscript -e ‘install.packages(“igraph”,repos=“https://cran.rstudio.com”)’ #not needed
+# Rscript -e ‘install.packages(“KernSmooth”,repos=“https://cran.rstudio.com”)’ #not needed
 
 
 
 
-pip install python-igraph #not needed
+# Rscript -e ‘source(“https://bioconductor.org/biocLite.R”);biocLite(“BiocInstaller”);biocLite(“BSgenome.Hsapiens.UCSC.hg19");biocLite(“TFBSTools”);BiocInstaller::biocLite(“GreenleafLab/chromVAR”)’
 
-pip install dash==0.21.0  # The core dash backend
-pip install dash-renderer==0.11.3  # The dash front-end
-pip install dash-html-components==0.9.0  # HTML components
-pip install dash-core-components==0.21.1  # Supercharged components
-pip install plotly --upgrade  # Plotly graphing library used in examples
-pip install gunicorn
+# pip install networkx==1.10
+# pip install shapely
+
+
+
+
+# pip install python-igraph #not needed
+
+# pip install dash==0.21.0  # The core dash backend
+# pip install dash-renderer==0.11.3  # The dash front-end
+# pip install dash-html-components==0.9.0  # HTML components
+# pip install dash-core-components==0.21.1  # Supercharged components
+# pip install plotly --upgrade  # Plotly graphing library used in examples
+# pip install gunicorn
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
